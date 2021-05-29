e1cf0460174125f67364ff136ad6cf818760e207
nead51 <Dustin.nead@gmail.com>
2021-05-29 14:08:14 -0400

    208

--- src/pages/step1.js
+++ src/pages/step1.js
@@ -22,24 +22,24 @@ const Step1 = () => {
           Each Player chooses a color
         </li>
         <li>
-          Each Player Gets a color tolkin, A Gold Let it Play Chip,  
+          Each Player Gets a color token, A Gold Let it Play Chip,  
         </li>
         <li>
-          One player is selected to DJ, they will attach their Phone to a Bluetooth Speaker, Distribute Bottlecaps and Select a Category from the Category Tab.
+          One player is selected to DJ, they will attach their Phone to a Bluetooth Speaker, Distribute Bottlecaps.
         </li>
         <li>
           Links to the Spotify playlist are in the Playlist tab
         </li>
         <li>
-          At the begining of each players's turn a category is selected.
-          The rest of the players add one song anonmously to that player's assigned color playlist, when all songs are added
-          the DJ plays the playlist and the Player awards a BottleCap to the song they think best matches the category.
+          At the begining of each players's turn they select a category. They are now the Judge.
+          The rest of the players add one song anonymously to that player's assigned color playlist, when all songs are added
+          the DJ plays the playlist and the Judge awards a BottleCap to the song they think best matches the category.
         </li>
         <li>
-          The DJ will only Play enough of the song for the player to know the song.  
+          The Judge will indicate to the DJ when they have heard enough of the song.  
         </li>
         <li>
-          And for the Gold Let it play Chip:  The player may use this chip to have the DJ play the whole song.  The Player who choose the song that uses the Let it play Tolkin 
+          And for the Gold Let it play Chip:  Anyone may use this chip to have the DJ play the whole song.  The Player who choose the song the Let it play Tolken is used on 
           is awarded 5 BottleCaps
         </li>
 
